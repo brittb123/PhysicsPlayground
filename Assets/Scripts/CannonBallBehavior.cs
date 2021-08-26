17c2c43aa8b34728a06815b9f09e98d484ed312d
Britt <brittboudreaux@yahoo.com>
2021-08-26 15:40:28 -0500

    Added cannonball and wall that breaks upon firing

---
+++ Assets/Scripts/CannonBallBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CannonBallBehavior : MonoBehaviour
+{
+    public float ForceonFire = 100;
+    private Rigidbody _rigidbody = null;
+    bool canFire = true;
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.isKinematic = true;
+    }
+    private void FixedUpdate()
+    {
+        if(Input.anyKeyDown && canFire)
+        {
+            _rigidbody.isKinematic = false;
+            _rigidbody.AddForce(transform.forward * ForceonFire);
+            canFire = false;
+        }
+    }
+
+}
