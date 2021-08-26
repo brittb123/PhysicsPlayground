52f6b8d6bfb79c979d23c624162dadfeb5437624
Britt <brittboudreaux@yahoo.com>
2021-08-26 12:13:29 -0500

    Added block that changes color if rigid body is sleeping or awake

---
+++ Assets/Scripts/WakeupMaterialBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class WakeupMaterialBehavior : MonoBehaviour
+{
+    public Material awakeMaterial = null;
+    public Material asleepMaterial = null;
+
+    private Rigidbody _rigidbody = null;
+    private MeshRenderer _render = null;
+
+    private bool _wasAsleep = false;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _render = GetComponent<MeshRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        //Set material to asleep if rigidbody is asleep
+        if (_wasAsleep && _rigidbody.IsSleeping() && asleepMaterial)
+        {
+            _wasAsleep = false;
+            _render.material = asleepMaterial;
+        }
+
+        else if (!_wasAsleep && !_rigidbody.IsSleeping() && awakeMaterial)
+        {
+            _wasAsleep = true;
+            _render.material = awakeMaterial;
+        }
+        //Set material to awake if rigidbody is awake
+    }
+}
