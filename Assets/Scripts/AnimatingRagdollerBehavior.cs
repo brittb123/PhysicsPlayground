614aa3b7a7ccf984326796d46bedb5430517547d
Britt <brittboudreaux@yahoo.com>
2021-09-03 15:53:48 -0500

    Unitychan added, ragdoll added to unitychan, fixed camera and animation

---
+++ Assets/Scripts/AnimatingRagdollerBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatingRagdollerBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _animator;
+
+    public bool ragodolltest;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (ragodolltest)
+            _animator.enabled = false;
+
+    }
+}
--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -9,20 +9,40 @@ public class PlayerBehavior : MonoBehaviour
     public float airControl = 1;
     public float gravityModifier = 1.0f;
     private bool _isGrounded = false;
+    public bool faceWithCamera;
 
     public Camera PlayerCamera;
+    public float Speed = 5.0f;
 
     [SerializeField]
     private CharacterController _playerController;
+
+    [SerializeField]
+    private Animator _animator;
 
     private Vector3 _desiredGroundVelocity;
     private Vector3 _desiredAirVelocity;
 
     private bool _needToJump;
 
+    public List<Rigidbody> rigidbodies = new List<Rigidbody>();
+
+    [SerializeField]
+    private bool _ragdolling;
+
     private void Awake()
     {
         _playerController.GetComponent<CharacterController>();
+    }
+
+    private void Ragdoll()
+    {
+        if (_ragdolling)
+        {
+            _desiredGroundVelocity = Vector3.zero;
+            _needToJump = false;
+            _animator.enabled = !_animator.enabled;
+        }
     }
 
     private void Update()
@@ -33,6 +53,8 @@ public class PlayerBehavior : MonoBehaviour
         //Sets the movement input from player
         _desiredGroundVelocity.x = Input.GetAxis("Horizontal");
         _desiredGroundVelocity.z = Input.GetAxis("Vertical");
+        float inputFoward = Input.GetAxis("Vertical");
+        float inputRight = Input.GetAxis("Horizontal");
 
         //Get camera normal
         Vector3 cameraForward = PlayerCamera.transform.forward;
@@ -63,7 +85,22 @@ public class PlayerBehavior : MonoBehaviour
         //Applies the gravity
         _desiredAirVelocity += Physics.gravity * gravityModifier * Time.deltaTime;
 
-      
+        if (faceWithCamera)
+        {
+            transform.forward = cameraForward;
+            _animator.SetFloat("Speed", inputFoward);
+            _animator.SetFloat("Direction", inputRight);
+        }
+        else
+        {
+            if(_desiredGroundVelocity != Vector3.zero)
+            {
+                transform.forward = _desiredGroundVelocity.normalized;
+            }
+            _animator.SetFloat("Speed", _desiredGroundVelocity.magnitude / Speed);
+        }
+        _animator.SetBool("Jump", !_isGrounded);
+        _animator.SetFloat("Vertical Speed", _desiredGroundVelocity.y / jumpStrength);
 
         _playerController.Move((_desiredGroundVelocity + _desiredAirVelocity) * Time.deltaTime);
 
@@ -75,5 +112,10 @@ public class PlayerBehavior : MonoBehaviour
 
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        _ragdolling = true;
+        Ragdoll();
+    }
 
 }
