ecceeb33670704ce1a8688273023ab5267a13c39
Britt <brittboudreaux@yahoo.com>
2021-09-16 16:54:31 -0500

    Cleaned up scene and made smaller, added jump pad prefab

---
+++ Assets/Scripts/Movement and Ragdoll/RotationWallBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationWallBehavior : MonoBehaviour
+{
+
+    public float x;
+    public float y;
+    public float z;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(x, y, z * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Movement and Ragdoll/SawBladeBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawBladeBehavior : MonoBehaviour
+{
+    [Tooltip("This will be the lower value border")]
+    [SerializeField]
+    private GameObject _border1;
+
+    [Tooltip("This is the greater value border")]
+    [SerializeField]
+    private GameObject _border2;
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x >= _border1.transform.position.x)
+        {
+            transform.position += new Vector3(1, 0, 0) * Time.deltaTime * speed;
+  
+        }
+        else if(transform.position.x <= _border2.transform.position.x)
+        {
+            transform.position += new Vector3(-1, 0, 0) * Time.deltaTime * speed;
+           
+        }
+    }
+}
