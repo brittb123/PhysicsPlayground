a6a3c64a9751870c28febc4dd38ed3f9fb5771a7
Britt <brittboudreaux@yahoo.com>
2021-09-23 15:32:43 -0500

    Made a release and fixed platform trigger behavior

--- Assets/Scripts/Movement and Ragdoll/PlatformTriggerBehavior.cs
+++ Assets/Scripts/Movement and Ragdoll/PlatformTriggerBehavior.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JumppadScript : MonoBehaviour
+public class PlatformTriggerBehavior : MonoBehaviour
 {
     [Tooltip("Gets the Render for the first platform")]
     public MeshRenderer platform1Render;
